a97b6ac03d9ef0336e87336eb0207f9bc9fc749e
Pmacdon15 <pmacdonald15@gmail.com>
2025-04-21 19:35:50 -0600

    update tos

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,9 +7,9 @@ export default function Home() {
       {/* <PaymentButton /> */}
       {/* <Header /> */}
       <div className="flex flex-col justify-between p-4 bg-white/20 backdrop-blur-md w-5/6 mt-8 border shadow-2xl h-4/6 text-center rounded-xl border-b border-white/10">
-        <p className="text-2xl">
+        <h1 className="text-2xl">
           Easily make a payment to Patrick MacDonald
-        </p>
+        </h1>
 
         <div className="mb-0">
           Please checkout our{" "}
--- src/app/terms-of-service/page.tsx
+++ src/app/terms-of-service/page.tsx
@@ -1,7 +1,9 @@
 export default function page() {
     return (
-        <>
-            test
-        </>
+        <div className="flex flex-col justify-between p-4 bg-white/20 backdrop-blur-md w-5/6 mt-8 border shadow-2xl h-4/6 text-center rounded-xl border-b border-white/10">
+            <h1 className="text-2xl">
+                Terms of service
+            </h1>
+        </div>
     );
 };
